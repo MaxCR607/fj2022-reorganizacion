ee1454d563d8b81a3e2b598baeda11b660b1dace
Maximiliano Castro Rodriguez <89948372+MaxCR607@users.noreply.github.com>
2022-03-29 08:13:58 -0700

    C13
    Definir la clase Mysql con las propiedades para conexion

--- app/mysql.php
+++ app/mysql.php
@@ -1,3 +1,13 @@
 <?php
 
-/* TODO: CREAR BASE DE DATOS */
+use \Mysqli;
+
+class Mysql extends \Mysqli {
+    protected $data = [
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'passwd' => '',
+        'dbname' => 'uadeo',
+        'port' => 3306
+    ];
+}
